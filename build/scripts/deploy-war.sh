638961f6bf97e3deadb9ce919d769359abb3b011
MGSE User <mgse@debian.mgse.hs-coburg.de>
2018-06-26 22:49:00 +0200

    deploy

--- build/scripts/deploy-war.sh
+++ build/scripts/deploy-war.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
 WAR_LOCATION=$(realpath "$1")
-WAR_FILE=$(realpath "$2")
-WAR_PATH ="$WAR_LOCATION/$WAR_FILE"
+WAR_FILE="$2"
+WAR_PATH="$WAR_LOCATION/$WAR_FILE"
 TOMCAT_SERVER=$(realpath "$3")
 TOMCAT_WAR_PATH="$TOMCAT_SERVER/webapps/$WAR_FILE"
 
 # Copy WAR
 echo "Copy $WAR_PATH to $TOMCAT_WAR_PATH"
+if [ ! -e "$WAR_PATH" ]; then
+	echo "File does not exist. Try to run your build script before deployment."
+	exit 1
+fi
 cp "$WAR_PATH" "$TOMCAT_WAR_PATH"
 
 # Create missing directory
-mkdir "$TOMCAT_SERVER/logs"
-
+if [ ! -d "$TOMCAT_SERVER/logs" ]; then
+	echo "Create missing directory 'logs'"
+	mkdir "$TOMCAT_SERVER/logs"
+fi
