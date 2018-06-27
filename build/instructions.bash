03e84fdb5dcf6370769db2c9058e3d3322eaaa6f
MGSE User <mgse@debian.mgse.hs-coburg.de>
2018-06-27 22:53:18 +0200

    Build script, instructions

--- build/instructions.bash
+++ build/instructions.bash
@@ -14,7 +14,10 @@ echo
 echo "2.1) Execute in terminal:"
 echo "xtext"
 echo
-echo "2.2) Select all projects and hit F5"
+echo "2.2) Select all projects"
+echo "- Hit F5"
+echo "- (In the Menu Bar) Project -> Clean..."
+echo "- (Bottom right) Hit the trash can to run the garbage collector"
 echo
 echo "2.3) Wait until workspace is ready (look at the bottom right) (Could take a while)"
 read -p "Press enter to continue"
@@ -24,7 +27,7 @@ echo "============================="
 echo "3) Start Runtime Application in Xtext"
 echo
 echo "3.1) Right click 'de.hs_coburg.mgse.platform.ser'"
-echo 
+echo
 echo "3.2) Run As -> Eclipse Application -> Ok"
 read -p "Press enter to continue"
 echo
@@ -35,7 +38,7 @@ echo
 echo "4.1) Right click 'de.hs_coburg.mgse.platform.sandbox'"
 echo
 echo "4.2) Refresh"
-echo 
+echo
 echo "4.3) (In the Menu Bar) Project -> Clean..."
 echo
 echo "4.4) Generated source files to:"
@@ -45,7 +48,7 @@ echo
 
 echo "============================="
 echo "5) Execute in terminal:"
-echo "build-deploy.sh"
+echo "sh build-deploy.sh"
 read -p "Press enter to continue"
 echo
 
