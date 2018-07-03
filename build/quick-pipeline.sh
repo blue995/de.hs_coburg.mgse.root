31a742d48ce6891f9e50b839d8f0a1c7788c35cd
MGSE User <mgse@debian.mgse.hs-coburg.de>
2018-07-03 18:49:19 +0200

    Stable build and application

--- build/quick-pipeline.sh
+++ build/quick-pipeline.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+. ./env.sh
 sh init-build.sh
+
 
 cd ../docker
 
+docker-compose down
 docker-compose build
-
-docker-compose up
+docker-compose up -d mysql-dev
+echo
+echo "Wait 5 Seconds"
+sleep 5
+docker-compose up admin restapi frontend
